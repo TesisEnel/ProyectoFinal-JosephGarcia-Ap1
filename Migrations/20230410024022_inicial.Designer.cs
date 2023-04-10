38bdaa9db8b9bf278ed971e394cda75182ccf297
JosephGT <joseph2002610@gmail.com>
2023-04-09 23:04:34 -0400

    Con todo funcional

--- Migrations/20230410024022_inicial.Designer.cs
+++ Migrations/20230410024022_inicial.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ProyectoFinal.Migrations
 {
     [DbContext(typeof(Contexto))]
-    [Migration("20230409211203_inicial")]
+    [Migration("20230410024022_inicial")]
     partial class inicial
     {
         /// <inheritdoc />
